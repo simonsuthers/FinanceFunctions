6d910b562b4461139547771cc6bbf9331c2f2b44
Simon Suthers <simonsuthers@yahoo.co.uk>
2023-09-20 23:36:45 +0100

    Added blob trigger

--- function_app.py
+++ function_app.py
@@ -23,3 +23,15 @@ def HttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+    
+
+@app.route(route="BlobTrigger")
+@app.blob_output(arg_name="outputblob", path="finance/test.txt", connection="AzureBlobStorage") 
+def BlobTrigger(req: func.HttpRequest, outputblob: func.Out[str]):
+    logging.info(f"Python blob trigger function processed blob")
+
+    clear_text = "hello"
+    
+    outputblob.set(clear_text)
+    
+    return func.HttpResponse(f"This HTTP triggered function executed successfully.")
