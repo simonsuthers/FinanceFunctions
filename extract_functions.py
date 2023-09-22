7d437f702e0ec622fa7d5a747f254652d9fe7824
Simon Suthers <simonsuthers@yahoo.co.uk>
2023-09-23 00:09:22 +0100

    Added code to get ticker

--- extract_functions.py
+++ extract_functions.py
@@ -28,22 +28,36 @@ def BlobTrigger(req: func.HttpRequest, outputblob: func.Out[str]):
     return func.HttpResponse(f"This HTTP triggered function executed successfully.")
 
 
-@bp.route(route="DfsTrigger", auth_level=func.AuthLevel.ANONYMOUS)
-def DfsTrigger(req: func.HttpRequest) -> func.HttpResponse:
+@bp.route(route="ticker", auth_level=func.AuthLevel.ANONYMOUS)
+def GetTicker(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+
+    #ticker = req.route_params.get('ticker')
+
+    ticker = req.params.get('ticker')
+    if not ticker:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            ticker = req_body.get('ticker')
+
+    if not ticker:
+        ticker = "IBM"
 
     credential = DefaultAzureCredential()
 
     apikey = os.environ["alphavantageapikey"]
     account_url = os.environ["FinanceDfsStorage"]
-    file_path = "file03.txt"
+    file_path = f"{ticker}.txt"
 
     file = DataLakeFileClient(account_url=account_url, 
                               credential=credential, 
                               file_system_name="finance", 
                               file_path=file_path)
     
-    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey={apikey}'
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&apikey={apikey}'
     r = requests.get(url)
     data = r.json()
     data = json.dumps(data)
@@ -52,22 +66,8 @@ def DfsTrigger(req: func.HttpRequest) -> func.HttpResponse:
     file.append_data(data, offset=0, length=len(data))
     file.flush_data(len(data))
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    return func.HttpResponse(f"Hello, {ticker}. This HTTP triggered function executed successfully.", status_code=200)
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
     
 
 @bp.function_name(name="my_second_function")
